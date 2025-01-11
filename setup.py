2e85f10617e46f424c45e577b6b1aaa707d37cff
Illia Stebelskyi <i.stebelskuy@gmail.com>
2025-01-11 14:51:44 +0800

    preparation for main release

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="nbg-currencies-api",
+    version="0.1.2",
+    description="API wrapper for fetching and normalizing NBG currency rates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Illia Stebelskyi",
+    author_email="illia.stebelski@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["requests"],
+    extras_require={
+        "async": ["aiohttp"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
